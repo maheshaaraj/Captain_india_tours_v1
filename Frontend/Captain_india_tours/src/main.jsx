cce6d542cae39e5b8d48833d7d34b26ba66096e4
mahesharaj <99277198+maheshaaraj@users.noreply.github.com>
2025-02-07 17:39:36 +0530

    routing

--- Frontend/Captain_india_tours/src/main.jsx
+++ Frontend/Captain_india_tours/src/main.jsx
@@ -5,12 +5,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
+  <StrictMode>
+    <BrowserRouter>
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
-    </StrictMode>,
-  </BrowserRouter>
-  
+    </BrowserRouter>
+  </StrictMode>,
 )
--- Frontend/Captain_india_tours/src/pages/PackagePage.jsx
+++ Frontend/Captain_india_tours/src/pages/PackagePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PackagePage = () => {
   return (
-    <div className='w-20 h-screen'>
-      <h1 className='font-bold text-2xl inline'>Hello From package page</h1>
+    <div>
+      <h1 className='font-bold text-2xl'>Hello From package page</h1>
     </div>
   )
 }
